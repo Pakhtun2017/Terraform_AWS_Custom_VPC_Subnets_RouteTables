9e202ef325e774de37ce03a72c1f53d27c26b4ac
pashtunzalmay <pashtunzalmay@pashtunzalmay-HP-Laptop-17-cn1xxx>
2024-02-21 08:48:10 -0500

    Removed IGW & NAT resources from main-route.tf to main.tf

--- main.tf
+++ main.tf
@@ -43,3 +43,34 @@ resource "aws_subnet" "private-subnets-tf" {
 }
 
 
+#Create an Internet gateway
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id = aws_vpc.vpc-tf.id
+  tags = {
+    Name = "terraform-igw"
+  }
+}
+
+#Create EIP for NAT gateway
+resource "aws_eip" "nat-gateway-eip" {
+  # vpc        = true
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.internet-gateway]
+
+  tags = {
+    Name = "terraform-nat-gw-eip"
+  }
+}
+
+#Create NAT gateway
+resource "aws_nat_gateway" "nat-gateway" {
+  depends_on    = [aws_subnet.public-subnets-tf]
+  allocation_id = aws_eip.nat-gateway-eip.id
+  subnet_id     = aws_subnet.public-subnets-tf["public-subnet-1"].id
+
+  tags = {
+    Name = "terraform-nat-gw"
+  }
+}
+
+
