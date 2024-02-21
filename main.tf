65ff090992b462410bbf0f76943428c9e3e345e9
pashtunzalmay <pashtunzalmay@pashtunzalmay-HP-Laptop-17-cn1xxx>
2024-02-20 20:18:17 -0500

    Second-commit: added public and private subnets

--- main.tf
+++ main.tf
@@ -8,3 +8,38 @@ resource "aws_vpc" "vpc-tf" {
     Name = "terraform-vpc"
   }
 }
+
+#Obtain available availability zones
+data "aws_availability_zones" "available-azs" {
+  state = "available"
+}
+
+#Create public subnets in each available availability zones
+resource "aws_subnet" "public-subnets-tf" {
+  for_each                = var.public-subnets
+  vpc_id                  = aws_vpc.vpc-tf.id
+  cidr_block              = cidrsubnet(var.vpc-cidr, 8, each.value + 100)
+  availability_zone       = tolist(data.aws_availability_zones.available-azs.names)[each.value - 1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "tf-subnet-public-${each.key}"
+    Tier = "public"
+  }
+}
+
+#Create private subnets in each available availability zones
+resource "aws_subnet" "private-subnets-tf" {
+  for_each                = var.private-subnets
+  vpc_id                  = aws_vpc.vpc-tf.id
+  cidr_block              = cidrsubnet(var.vpc-cidr, 8, each.value)
+  availability_zone       = tolist(data.aws_availability_zones.available-azs.names)[each.value - 1]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "tf-subnet-private-${each.key}"
+    Tier = "private"
+  }
+}
+
+
--- variables.tf
+++ variables.tf
@@ -17,3 +17,26 @@ variable "true" {
   type    = bool
   default = true
 }
+
+variable "public-subnets" {
+  default = {
+    "public-subnet-1" = 1
+    "public-subnet-2" = 2
+    "public-subnet-3" = 3
+    "public-subnet-4" = 4
+    "public-subnet-5" = 5
+    "public-subnet-6" = 6
+  }
+}
+
+variable "private-subnets" {
+  default = {
+    "private-subnet-1" = 1
+    "private-subnet-2" = 2
+    "private-subnet-3" = 3
+    "private-subnet-4" = 4
+    "private-subnet-5" = 5
+    "private-subnet-6" = 6
+  }
+
+}
